1e98665eecc1327d45f577e75f91231ca228c622
Serhii Onishchuk <onixsonic@gmail.com>
2017-11-10 12:29:38 +0200

    Send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,9 +93,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV['gmail_username'],
-    password: ENV['gmail_password'],
-    authentication: 'heroku.com',
+    user_name: ENV['GMAIL_SMTP_USER'],
+    password: ENV['GMAIL_SMTP_PASSWORD'],
+    authentication: :plain,
     enable_starttls_auto: true
   }
 
