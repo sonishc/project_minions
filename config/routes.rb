ac0c4a74b6fa265334d25c2a467a4d6921658f5f
Serhii Onishchuk <onixsonic@gmail.com>
2017-11-19 23:38:37 +0200

    Write right routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: :show do
-    resources :toys
+    resources :toys do
+      member do
+        get '/event_switch', to: 'toys#event_switch'
+        post '/send_history', to: 'toys#send_history'
+      end
+    end
     member do
       get '/confirm_email', to: 'users#confirm_email'
     end
   end
-  post '/event_switch', to: 'toys#event_switch'
-  post '/send_history', to: 'toys#send_history'
   root to: 'homes#index'
 end
