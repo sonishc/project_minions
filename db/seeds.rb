e6c380644c43e7059b319c04e661760b648d27e5
Serhii Onishchuk <onixsonic@gmail.com>
2017-11-21 13:45:53 +0200

    Change seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create(
+  name: 'admin'
+)
+
 User.create(
   email: 'onixsonic@gmail.com',
   name: 'Serhii',
