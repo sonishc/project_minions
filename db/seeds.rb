d7561af6779a219757ac120950d588f08527db4b
Serhii Onishchuk <onixsonic@gmail.com>
2017-11-21 13:54:17 +0200

    Fix rake db:seed in production and vendor assets:precompile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Role.create(
   name: 'admin'
 )
@@ -19,4 +11,9 @@ User.create(
   password_confirmation: ENV['USER_PASSWORD_DEV'],
   email_confirmed: true,
   confirm_token: nil
+)
+
+Assignment.create(
+  user_id: 1,
+  role_id: 1
 )
