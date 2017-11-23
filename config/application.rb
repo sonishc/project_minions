76ed5dda958dfa45dca04b3f27ce6004d418f296
Serhii Onishchuk <onixsonic@gmail.com>
2017-11-23 09:57:04 +0200

    aplication.rb assets

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,9 @@ Bundler.require(*Rails.groups)
 
 module Minion
   class Application < Rails::Application
-
     config.load_defaults 5.1
-    config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
     config.active_job.queue_adapter = :delayed_job
     config.action_mailer.perform_caching = true
-
   end
 end
